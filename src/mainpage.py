47d4623b346599e494c0f2d9182d96be10240822
Totoro10101 <67016573+Totoro10101@users.noreply.github.com>
2022-02-24 10:11:14 -0800

    updated mainpage with image lines

--- src/mainpage.py
+++ src/mainpage.py
@@ -17,11 +17,16 @@
     @subsection subsec_sch1 task_encoder
                             Reads the position of the encoder on each motor and stores them in a share.
                             
+                            @image html Task_Encoder.jpg
+                            
     @subsection subsec_sch2 task_startup
                             This task will have the ability to zero the encoder position.
                             Zeroing the encoder is important to ensuring we start plotting the
                             given image at a known location. It will use a limit switch to determine the
                             zero point on the drawing area. 
+                            
+                            @image html Task_Startup.jpg
+                            @image html Task_Startup_FSM.jpg
                             
     @subsection subsec_sch3 task_controller
                             Controls the motor duty cycle based on set points received from the parser task.
@@ -29,11 +34,15 @@
                             current encoder position. Then it uses closed loop control to set the motor duty
                             cycle and move the pen to the desired location. This task will read from csv data
                             to move the pen carriage by controlling each motor.
-    
+                            
+                            @image html Task_Controller.jpg
+                            
     @subsection subsec_sch4 task_parser
                             Parses the HPGL file for a given image and then parses this file for the relevant
                             commands to move the pen carriage. These include "pen advance" and lifting the pen
-                            off of the paper when not plotting. 
+                            off of the paper when not plotting.
+                            
+                            @image html Task_Parser.jpg
                          
     @author             Tori Bornino
     @author             Jackson McLaughlin
