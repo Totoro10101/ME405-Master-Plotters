663fee3d2c9fd6b42555363b96db31bbd2494933
Totoro10101 <67016573+Totoro10101@users.noreply.github.com>
2022-02-24 10:04:56 -0800

    Update mainpage.py

--- src/mainpage.py
+++ src/mainpage.py
@@ -13,16 +13,27 @@
     
     @section sec_sch    Software Design
                         
-    @subsection subsec_sch task_motor
-    
     
     @subsection subsec_sch1 task_encoder
+                            Reads the position of the encoder on each motor and stores them in a share.
+                            
+    @subsection subsec_sch2 task_startup
+                            This task will have the ability to zero the encoder position.
+                            Zeroing the encoder is important to ensuring we start plotting the
+                            given image at a known location. It will use a limit switch to determine the
+                            zero point on the drawing area. 
+                            
+    @subsection subsec_sch3 task_controller
+                            Controls the motor duty cycle based on set points received from the parser task.
+                            When a set point is taken from the parser task, it compares the set point to the
+                            current encoder position. Then it uses closed loop control to set the motor duty
+                            cycle and move the pen to the desired location. This task will read from csv data
+                            to move the pen carriage by controlling each motor.
     
-    
-    @subsection subsec_sch2 task_controller
-    
-    
-    @subsection subsec_sch3 task_gcode
+    @subsection subsec_sch4 task_parser
+                            Parses the HPGL file for a given image and then parses this file for the relevant
+                            commands to move the pen carriage. These include "pen advance" and lifting the pen
+                            off of the paper when not plotting. 
                          
     @author             Tori Bornino
     @author             Jackson McLaughlin
