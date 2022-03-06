01372eff09ba6cd8bf150459580e42793a54fea4
Totoro10101 <67016573+Totoro10101@users.noreply.github.com>
2022-03-06 12:53:57 -0800

    added transformation math to get the value in ticks

--- src/task_parser.py
+++ src/task_parser.py
@@ -81,31 +81,81 @@ class Parser:
             n = 3
             i = 0
             _prev_com = self._up  # Stores the previous pen command of up or down
+            _r_pulley = 16*2/3.14159    # mm
+            _PPR = 256*4*16
             coord_queue = task_share.Queue('i', 10000, thread_protect = True, overwrite = False)
             
             while i <= len(all_coords):
                 if all_coords[i] == 'PU':  
                     n = 3
-                    r1 = int(all_coords[i+1])/40  #convert dpi from hpgl to mm
-                    r2 = int(all_coords[i+2])/40
-                    coord_queue.put((r1, r2, self._up))
+                    x1 = int(all_coords[i+1])/40  #convert dpi from hpgl to mm
+                    y1 = int(all_coords[i+2])/40
+                    calc = transform(x1,y1)
+                    r1 = calc[0]
+                    r2 = calc[1]
+                    theta_rad1 = r1/_r_pulley
+                    theta_rad2 = r2/_r_pulley
+                    tix1 = theta_rad1*_PPR
+                    tix2 = theta_rad2*_PPR
+                    coord_queue.put((tix1, tix2, self._up))
                     _prev_com = self._up
                     i = i + 3
                 elif all_coords[i] == 'PD':
                     n = 3
-                    r1 = int(all_coords[i+1])/40
-                    r2 = int(all_coords[i+2])/40
-                    coord_queue.put((r1, r2, self._down))
+                    x1 = int(all_coords[i+1])/40
+                    y1 = int(all_coords[i+2])/40
+                    calc = transform(x1,y1)
+                    r1 = calc[0]
+                    r2 = calc[1]
+                    theta_rad1 = r1/_r_pulley
+                    theta_rad2 = r2/_r_pulley
+                    tix1 = theta_rad1*_PPR
+                    tix2 = theta_rad2*_PPR
+                    coord_queue.put((tix1, tix2, self._down))
                     _prev_com = self._down
                     i = i + 3
                 else:
                     n = 2
-                    r1 = int(all_coords[i])/40
-                    r2 = int(all_coords[i+1])/40
-                    coord_queue.put((r1, r2, _prev_com))
+                    x1 = int(all_coords[i])/40
+                    y1 = int(all_coords[i+1])/40
+                    calc = transform(x1,y1)
+                    r1 = calc[0]
+                    r2 = calc[1]
+                    theta_rad1 = r1/_r_pulley
+                    theta_rad2 = r2/_r_pulley
+                    tix1 = theta_rad1*_PPR
+                    tix2 = theta_rad2*_PPR
+                    coord_queue.put((tix1, tix2, _prev_com))
                     i = i + 2
             return coord_queue
-            
+
+R = 263 #mm             # Distance between the motors
+y_home = 2*A_home/R
+x_home = (r_1_home**2 - y_home**2)**0.5
+
+def transform(x, y):
+    '''!
+    This transform calculates the two motor angles on the board given some
+    point within the drawing grid.
+    
+    The point is calculated with respect to the corner closest to motor 1.
+    This assumes the distance between the motors is R = 263mm and the "zeroed"
+    length of each motor is r = 330 mm (fully extended to reach the furthest
+    corners) which results in a homed location of
+    (x_home, y_home) = (182.3, 122.7) mm.
+    
+    @param x the x coordinate
+    @param y the y coordinate
+    @return the motor angles (r_1, r_2) in (mm)
+    '''
+    # we can then calcualte the radius (mm) needed to reach any point
+    # x,y (mm) on the board
+
+    r_1_pprime = ( (y_home+y)**2 + (x_home-x)**2)**0.5
+    r_2_pprime = ( (y_home+y)**2 + (R-(x_home-x))**2)**0.5
+
+    return(r_1_pprime, r_2_pprime)
+
 if __name__ == '__main__':
     parser = Parser()
     parser.read()
