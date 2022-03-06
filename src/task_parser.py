5292f371c5e0ff2f61f6c659b5f95c139cc8cf70
Totoro10101 <67016573+Totoro10101@users.noreply.github.com>
2022-03-05 17:16:36 -0800

    Update task_parser.py

--- src/task_parser.py
+++ src/task_parser.py
@@ -27,7 +27,7 @@ def read():
                 separate_commands = split_hpgl[i].split(',')  # split the individual items in the list s
                                                               # since HPGL puts out long continuous commands
                                                               # as a csv essentially.
-                print(separate_commands)
+                # print(separate_commands)
                 for j in range(len(separate_commands)): # iterate through the list created by splitting at the commas
                     if len(separate_commands) >= 2:      # if it's got a length of more than 2, it shows that the pen plotter will be
                                                         # continuously moving in that pen state
@@ -39,6 +39,35 @@ def read():
                         for n in range(len(separate_commands)):
                             all_coords.append(separate_commands[n])  # further split the values to not put lists inside of lists
         print(all_coords)
+        new_coords = []
+        twodim_array = []
+        for i in range(len(all_coords)):
+            if 'IN' in all_coords[i]:
+                # print("Initialize")
+                pass
+            elif 'PU' in all_coords[i]:
+                # print("Pen Up")
+                sep = all_coords[i].split('PU') # Removes PU from the command, will replace with 0
+                sep[0] = str(0)  # Pen is not touching the paper
+                # print(sep)
+                if sep[1] is '': # Pass the element if it is empty after split
+                    pass
+                else:
+                    new_coords.append(sep[0])  # Add pen up command to the new coordinates
+                    new_coords.append(sep[1])  # Add the coordinate associated with it.
+            elif 'SP1' in all_coords[i]:
+                # print("Select Pen")
+                pass
+            elif 'PD' in all_coords[i]:
+                # print("Pen Down")
+                sep2 = all_coords[i].split('PD') # Removes PD and replaces with 1 to indicate pen is touching paper
+                sep2[0] = str(1)
+                # print(sep2)
+                new_coords.append(sep2[0])
+                new_coords.append(sep2[1])
+            else:
+                new_coords.append(all_coords[i])
+        print(new_coords)
         
 if __name__ == '__main__':    
     read()
