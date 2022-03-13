61e5a617eeb8cee98e89612d51752462fe8bb175
tbornino <vbornino@calpoly.edu>
2022-03-13 16:45:05 -0700

    Update Main Page
    Update Main Page to reflect code changes

--- src/mainpage.py
+++ src/mainpage.py
@@ -12,20 +12,28 @@
                         https://github.com/Totoro10101/ME405-Master-Plotters
     
     @section sec_sch    Software Design
-                        This pen pen plotter has the structure:
+                        Before the pen plotter draws the given image, the plotter
+                        is zeroed and the given HPGL is parsed.
+                        
+                        During general operation the pen plotter has two main tasks:
+                        encoder and controller. The encoders running consecutively read 
+                        the motor positions and the controller uses the positions to
+                        controll the motors. The desired plotter position is loaded
+                        during startup. 
                         @image html task_diagram.jpg width=75% \n
                             
     @subsection subsec_sch1 Startup
                             Before the pen plotter plots the drawing, it will zero its position. 
                             Zeroing the encoder is important to ensuring we start plotting the
                             given image at a known location. It utilizes two limit switches to 
-                            determine thezero point on the drawing area. 
+                            determine the carriage zero point where the plotter carriage is
+                            completely extended. 
                             
                             @image html task_startup_FSM.jpg width=75% \n
                             
     @subsection subsec_sch2 task_parser
-                            Parses the HPGL file for a given image outputting the required pen locations and
-                            pen states  to construct the drawing. These include "pen advance" and lifting the pen
+                            Parses the HPGL file for a given image outputting the required pen locations in ticks and
+                            pen states to construct the drawing. These include "pen advance" and lifting the pen
                             off of the paper when not plotting. Parser task doesn't require a finite state machine
                             since it's only purpose is to read the HPGL data and doesn't transition to any other state.
                             However, the kinematic equations used in the derivation of the transition are further
@@ -33,15 +41,16 @@
                             
     @subsection subsec_sch3 task_encoder
                             Reads the position of the encoder on each motor and stores them in a share.
-                            This task does not require a finite state machine since it is only sensing
+                            This task does not require a finite state machine since it is only reading
                             the position of the motor and the zeroing takes place in the startup task.
                             
     @subsection subsec_sch4 task_controller
                             Controls the motor duty cycle and servo position based on set points stored in the  
                             set point queues. The controller task has two states: Motor and Servo. When a new set
                             point is loaded, the pen condition is checked, and corrected, before transitioning
-                            back to the motor state. The controller driver uses PID closed loop control to set 
-                            the motor duty cycle and move the pen to the desired location. 
+                            back to the motor state until the next desired position is reached. The controller
+                            driver uses PID closed loop control to set the motor duty cycle and move the pen
+                            to the desired location. 
                             
                             @image html task_controller_FSM.jpg width=75% \n
                             
