f5aa4c702c10a762fefba27398495f8a46cbb5cb
jacksonmcl <jmclau05@calpoly.edu>
2022-03-13 14:16:26 -0700

    servo cleanup

--- src/servo.py
+++ src/servo.py
@@ -8,35 +8,26 @@
 '''
 
 import pyb
-import math
 
 class Servo:
     """!
     This class contains methods to control the angle of a servo.
     """
-    
-    
-    def __init__ (self, pin1 = pyb.Pin.board.PA6, timer = pyb.Timer(3, freq=50), timerChannel = 1 ):
+    def __init__(self, pin1=pyb.Pin.board.PA6, timer=pyb.Timer(3, freq=50),
+                 channel=1):
         """!
         This instantiates a servo object.
         """   
-      
         self._pin1 = pyb.Pin(pin1, pyb.Pin.OUT_PP)
-        
         self._timer = timer
-        
-        self._channel = self._timer.channel(timerChannel, pyb.Timer.PWM, pin=pin1)
+        self._channel = self._timer.channel(channel, pyb.Timer.PWM, pin=pin1)
         
     def set_angle(self, angle):
-        
         self._channel.pulse_width_percent(angle)
         
-
+# Test program for servos
 if __name__ == "__main__":
-    
     import time
-    
-      
     servo1 = Servo()
     time.sleep(1)
     print('moving')
@@ -44,7 +35,4 @@ if __name__ == "__main__":
     time.sleep(3)
     servo1.set_angle(8)
     time.sleep(0.5)
-#     servo1.set_angle(0.5)
     print('moved')
-#     time.sleep(5)
-#     servo1.angle()
