cde7c2572415418af3d4e6ccfc14a07588add06c
tbornino <vbornino@calpoly.edu>
2022-03-13 15:47:34 -0700

    Update servo.py
    Finalize the doxygen for servo

--- src/servo.py
+++ src/servo.py
@@ -13,16 +13,26 @@ class Servo:
     """!
     This class contains methods to control the angle of a servo.
     """
-    def __init__(self, pin1=pyb.Pin.board.PA6, timer=pyb.Timer(3, freq=50),
-                 channel=1):
+    def __init__(self, pin1=pyb.Pin.board.PA9, timer=pyb.Timer(1, freq=50),
+                 channel=2):
         """!
         This instantiates a servo object.
+        
+        @param pin1     The servo signal pin, default pyb.Pin.board.PA9
+        @param timer    The servo tmer, default pyb.Timer(1, freq=50)
+        @param channel  The timer channel ID, default 2
         """   
         self._pin1 = pyb.Pin(pin1, pyb.Pin.OUT_PP)
         self._timer = timer
         self._channel = self._timer.channel(channel, pyb.Timer.PWM, pin=pin1)
         
+        
     def set_angle(self, angle):
+        """!
+        This method sets the servo angle.
+        
+        Valid range is between 2-13 with the center at 7.5 
+        """
         self._channel.pulse_width_percent(angle)
         
 # Test program for servos
