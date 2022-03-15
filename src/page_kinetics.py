e73dd01c8d143d6f47aec4954df873b220b3f925
tbornino <vbornino@calpoly.edu>
2022-03-15 13:30:32 -0700

    Create page_kinetics.py

---
+++ src/page_kinetics.py
@@ -0,0 +1,7 @@
+''' @file                page_kinetics.py
+    @brief               Page describing the transform used to convert from (x,y) to (theta_1, theta_2).
+    
+    \page transform Kinemmatic Derivations of Transform
+    
+    \image html Kptuneplotall.png
+'''
